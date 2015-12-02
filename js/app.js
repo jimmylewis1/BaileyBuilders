328b5ff20ce7ba5bd562801fe26c4ebffe23f7b9
jimmylewis1 <jimmy@getweave.com>
2015-12-01 20:22:36 -0700

    layout fixes

--- js/app.js
+++ js/app.js
@@ -2,34 +2,35 @@ var app=angular.module('bailey', ['ui.router']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
    
+    $urlRouterProvider
+        .otherwise('/');
+    
     $stateProvider
         .state('home', {
-            url: '/',
-            templateUrl: 'js/home/homeTmpl.html',
-            controller: 'homeCtrl'
+           url: '/',
+           controller: 'homeCtrl'
         })
         .state('about', {
             url: '/about',
-            templateUrl: 'js/about/aboutTempl.html',
-            controller: 'aboutCtrl'
+            templateUrl: 'js/about/aboutTmpl.html',
+            //controller: 'aboutCtrl'
         })
         .state('services', {
             url: '/services',
-            templateUrl: 'js/services/servicesTemp.html',
-            controller: 'servicesCtrl'
+            templateUrl: 'js/services/servicesTmpl.html',
+            //controller: 'servicesCtrl'
         })
         .state('projects', {
             url: '/projects',
-            tempalteUrl: 'js/projects/projectsTempl.html',
-            controller: 'projectsCtrl'
+            tempalteUrl: 'js/projects/projectsTmpl.html',
+            //controller: 'projectsCtrl'
         })
         .state('contact', {
             url: '/contact',
-            templateUrl: 'js/contact/contactTempl.html',
-            controller: 'contactCtrl'
+            templateUrl: 'js/contact/contactsTmpl.html',
+            //controller: 'contactsCtrl'
         });
     
-    $urlRouterProvider
-        .otherwise('/');
+    
     
 });
